43817d3c9d451539e56e718439f08cb011c311d7
anderssonvivi <viviandersson0@gmail.com>
2023-12-05 11:30:01 +0100

    deployment to hardhat network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+
+/* run a script with `npx hardhat run <script>`.
+  hardhat will compile your contracts, add the Hardhat Runtime Environment's members to the
+  global scope, and execute the script. 
+  
+  deploy scripts are run using `hh run <path-to-script>` assuming `hh` shortcut installed.
+  e.g. `hh run scripts/deploy.js` */
+
+
+const hre = require("hardhat"); // can also run scripts via node <script>
 
 async function main() {
 
-  /* sample deployment code using ethers
-  
   const fats = await hre.ethers.deployContract("FaTS");
-
   await fats.waitForDeployment();
-
-  console.log(
-    `FATS deployed to ${fats.target}`
-  );*/
+  console.log(`FaTS deployed to ${fats.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
+// pattern to use async/await everywhere and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
