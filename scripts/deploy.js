63b0aa1d66815bec52a1866989eb5abeae287381
anderssonvivi <viviandersson0@gmail.com>
2023-12-05 12:31:20 +0100

    deployemt configs for sepolia network 🕷️

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,17 +3,18 @@
   hardhat will compile your contracts, add the Hardhat Runtime Environment's members to the
   global scope, and execute the script. 
   
-  deploy scripts are run using `hh run <path-to-script>` assuming `hh` shortcut installed.
-  e.g. `hh run scripts/deploy.js` */
+  deploy scripts run using `hh run <path-to-script> --network <network-name>`
+  (assuming `hh` shortcut installed).
+  e.g. `hh run scripts/deploy.js` for hardhat local network */
 
 
 const hre = require("hardhat"); // can also run scripts via node <script>
 
 async function main() {
-
+  const [deployer] = await ethers.getSigners();
   const fats = await hre.ethers.deployContract("FaTS");
   await fats.waitForDeployment();
-  console.log(`FaTS deployed to ${fats.target}`);
+  console.log(`FaTS deployed to ${fats.target} from account: ${deployer.address}`);
 }
 
 // pattern to use async/await everywhere and properly handle errors.
