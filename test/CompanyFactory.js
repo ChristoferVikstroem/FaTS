6ba95fbb1cc01ae97687aa2d79789e9375683dbe
anderssonvivi <viviandersson0@gmail.com>
2023-12-17 16:55:53 +0100

    fixes all but one test

--- test/CompanyFactory.js
+++ test/CompanyFactory.js
@@ -7,8 +7,6 @@ const { expect } = chai;
 describe('CompanyFactory', function () {
     const companyName = 'Google';
     const sector = 'IT';
-    const employeeTitle = 'Software Engineer';
-    const salary = 60000;
 
     async function defaultFixture() {
         const [factoryOwner, companyAccount, employee1, employee2] = await ethers.getSigners();
@@ -28,11 +26,12 @@ describe('CompanyFactory', function () {
     async function manyGrantedFixture() {
         const [factoryOwner, company1, company2, company3, employee1, employee2] = await ethers.getSigners();
         const companyFactory = await ethers.deployContract("CompanyFactory");
-        const companyAddress = await companyAccount.address;
-        await companyFactory.grantRegistryRight(company1, 'Google', sector);
-        await companyFactory.grantRegistryRight(company2, 'Noogle', sector);
-        await companyFactory.grantRegistryRight(company3, 'The Coffice', 'Fine Dining');
-        return { companyFactory, factoryOwner, companyAddress, company1, company2, company3, employee1, employee2 };
+        const sector1 = 'IT';
+        const sector2 = 'Fine Dining';
+        await companyFactory.grantRegistryRight(company1, 'Google', sector1);
+        await companyFactory.grantRegistryRight(company2, 'Noogle', sector1);
+        await companyFactory.grantRegistryRight(company3, 'The Coffice', sector2);
+        return { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2, sector1, sector2 };
     }
 
     async function registeredFixture() {
@@ -43,12 +42,14 @@ describe('CompanyFactory', function () {
         await companyFactory.connect(companyAccount).registerCompany(companyAddress);
         return { companyFactory, companyAccount, employee1, employee2, factoryOwner, companyAddress };
     }
+
     async function manyRegisteredFixture() {
-        const { factoryOwner, company1, company2, company3, employee1, employee2 } = await loadFixture(manyGrantedFixture);
+        const { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2, sector1, sector2 } = await loadFixture(manyGrantedFixture);
         await companyFactory.connect(company1).registerCompany(company1.address);
+
         await companyFactory.connect(company2).registerCompany(company2.address);
         await companyFactory.connect(company3).registerCompany(company3.address);
-        return { companyFactory, factoryOwner, companyAddress, company1, company2, company3, employee1, employee2 };
+        return { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2, sector1, sector2 };
     }
 
 
@@ -150,6 +151,7 @@ describe('CompanyFactory', function () {
         });
     });
 
+
     describe('Removing a Company', function () {
         it('should not be possible for an unregistered company', async function () {
             const { companyFactory, companyAddress, factoryOwner } = await loadFixture(defaultFixture);
@@ -157,11 +159,23 @@ describe('CompanyFactory', function () {
         });
 
         it('should be possible for the Company owner', async function () {
-            const { companyFactory, companyAccount, companyAddress } = await loadFixture(registeredFixture);
-            await companyFactory.connect(companyAccount).removeCompany(companyAddress)
-            await expect(companyFactory.getCompanyDetails(companyAddress)).to.be.revertedWith("No such company registered.");
+            const { companyFactory, company1 } = await loadFixture(manyRegisteredFixture);
+            await companyFactory.connect(company1).removeCompany(company1.address);
+            await expect(companyFactory.getCompanyDetails(company1)).to.be.revertedWith("No such company registered.");
         });
 
+        it('should remove the company from the sector', async function () {
+            // test fails
+            const { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2, sector1, sector2 } = await loadFixture(manyRegisteredFixture);
+
+            const events = await companyFactory.queryFilter(
+                companyFactory.filters.CompanyRegistered()
+            );
+            const companyContract = await ethers.getContractAt('Company', events[0].args[1]); // Company contract instance
+            await companyFactory.connect(company1).removeCompany(company1.address);
+            const sectorAddresses = await companyFactory.getCompanyAddressesInSector(sector1);
+            await expect(sectorAddresses).to.deep.equal([company2.address]);
+        });
 
         it('should be possible for the Factory owner', async function () {
             const { companyFactory, factoryOwner, companyAddress } = await loadFixture(registeredFixture);
@@ -178,47 +192,40 @@ describe('CompanyFactory', function () {
             var details = await companyFactory.getCompanyDetails(companyAddress);
             await expect([details[0], details[1], Number(details[2]), Number(details[3])]).to.deep.equal([companyName, sector, Number(0), Number(0)]);
         });
-
         it('should return correct details for a registered company with added employees', async function () {
-            const { companyFactory, companyAccount, companyAddress, employee1, employee2 } = await loadFixture(grantedFixture);
-            const registerTransaction = await companyFactory.connect(companyAccount).registerCompany(companyAddress);
-            await registerTransaction.wait();
+            const { companyFactory, companyAccount, companyAddress, employee1, employee2 } = await loadFixture(registeredFixture);
+            // contract address from logged event
+            const [event] = await companyFactory.queryFilter(
+                companyFactory.filters.CompanyRegistered()
+            );
+            const company = await ethers.getContractAt('Company', event.args[1]); // Company contract instance
 
-            // Ensure that the companies mapping is properly populated after registration
-            const companyObject = companyFactory.companies[companyAddress];
-            await 
-            const company = await ethers.getContractAt("Company", address(companyFactory.companies[companyAddress]));
-
-            await company.connect(companyAccount).addEmployee(employee1.address, "Sales", 42000);
-            await company.connect(companyAccount).addEmployee(employee2.address, "Event Manager", 100000);
-
-            const details = await companyFactory.getCompanyDetails(companyAddress);
-
-            // Assuming companyName and sector are defined somewhere in your code
-            await expect([details[0], details[1], Number(details[2]), Number(details[3]) / 2])
-                .to.deep.equal([companyName, sector, Number(2), Number((42000 + 100000) / 2)]);
+            await company.connect(companyAccount).addEmployee(employee1.address, 'Sales', 42000);
+            await company.connect(companyAccount).addEmployee(employee2.address, 'Event Manager', 100000);
+            const details = await companyFactory.getCompanyDetails(companyAddress); // company name, sector, num employees, avg salary
+            expect(details[0]).to.equal(companyName);
+            expect(details[1]).to.equal(sector);
+            expect(Number(details[2])).to.equal(2);
+            expect(Number(details[3])).to.equal((42000 + 100000) / 2);
         });
-
-
     });
 
     describe('Querying Company addresses in a sector', function () {
         it('should return no addresses for sector with no companies', async function () {
             const { companyFactory } = await loadFixture(defaultFixture);
-            const companies = await companyFactory.getCompanyAddressesInSector('NonexistentSector');
-            expect(companies).to.deep.equal([]);
+            const companies = await companyFactory.getCompanyAddressesInSector('Scooby');
+            expect(companies).to.be.empty;
         });
+
         it('should handle registering multiple companies in different sectors', async function () {
-            const { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2 } = await loadFixture(manyGrantedFixture);
-            await companyFactory.connect(company1).registerCompany(company1.address);
-            await companyFactory.connect(company2).registerCompany(company2.address);
-            await companyFactory.connect(company3).registerCompany(company3.address);
-            const ITSector = await companyFactory.getCompanyAddressesInSector('IT');
-            const sector2 = await companyFactory.getCompanyAddressesInSector('Fine Dining');
-            expect(ITSector).to.deep.equal([company1.address, company2.address]);
-            expect(sector2).to.deep.equal([company3.address]);
+            const { companyFactory, company1, company2, company3, sector1, sector2 } = await loadFixture(manyRegisteredFixture);
+            const IT = await companyFactory.getCompanyAddressesInSector(sector1);
+            const FD = await companyFactory.getCompanyAddressesInSector(sector2);
+            expect(IT).to.deep.equal([company1.address, company2.address]);
+            expect(FD).to.deep.equal([company3.address]);
         });
     })
+
 
     describe('Querying average salaries in sector', function () {
         it('should return 0 for sector with no companies', async function () {
@@ -233,19 +240,44 @@ describe('CompanyFactory', function () {
             expect(averageSalary).to.equal(0);
         });
 
-        it('should revert for a sector with no companies', async function () { // todo should it really??
-            const { companyFactory } = await loadFixture(defaultFixture);
-            expect(await companyFactory.getAverageSalaryInSector('fakeTestSector')).to.be.revertedWith('The sector has no registered companies.');
+        it('should return correct result for multiple companies in a sector', async function () {
+            const { companyFactory, company1, company2, company3, sector1, employee1, employee2 } = await loadFixture(manyGrantedFixture);
+            //const contract1 = await ethers.getContractAt("Company", company1.add);
+            await companyFactory.connect(company1).registerCompany(company1.address);
+            await companyFactory.connect(company2).registerCompany(company2.address);
+            await companyFactory.connect(company3).registerCompany(company3.address);
+
+            const events = await companyFactory.queryFilter(
+                companyFactory.filters.CompanyRegistered()
+            );
+            const salaries = [42000, 30000]; // make global
+            const titles = ['Lizard', 'Moth'];
+            const owners = [company1, company2];
+
+            for (let i = 0; i < 2; i++) {
+                const contractAddress = events[i].args[1];
+                const companyInstance = await ethers.getContractAt('Company', contractAddress);
+                await companyInstance.connect(owners[i]).addEmployee(employee1.address, titles[i], salaries[i]);
+                await companyInstance.connect(owners[i]).addEmployee(employee2.address, titles[i], salaries[i]);
+            }
+
+            const averageSalary1 = await expect(companyFactory.getAverageSalaryInSector(sector1)).to.be.fulfilled;
+            expect(Number(averageSalary1)).to.equal((salaries[0] + salaries[1]) / 2);
         });
 
-        it('should handle getAverageSalaryInSector with multiple companies', async function () {
-            const { companyFactory, factoryOwner, company1, company2, company3, employee1, employee2 } = await loadFixture(manyRegisteredFixture);
-            //const contract1 = await ethers.getContractAt("Company", company1.add);
-            await company.connect(companyAccount).addEmployee(employee1.address, "CEO", 10000);
-            await company.connect(companyAccount).addEmployee(employee2.address, "CTO", 10000);
-            const averageSalary = await expect(companyFactory.getAverageSalaryInSector(sector)).to.be.fulfilled;
-            const expectedSalary = 10000;
-            expect(Number(averageSalary)).to.equal(expectedSalary);
+        it('should return correct result for one company in a sector', async function () {
+            const { companyFactory, company3, sector2, employee1, employee2 } = await loadFixture(manyGrantedFixture);
+
+            await companyFactory.connect(company3).registerCompany(company3.address);
+            const [event] = await companyFactory.queryFilter(
+                companyFactory.filters.CompanyRegistered()
+            );
+            const company = await ethers.getContractAt('Company', event.args[1]);
+            const salaries = [42000, 27000];
+            await company.connect(company3).addEmployee(employee1.address, 'Sales', salaries[0]);
+            await company.connect(company3).addEmployee(employee2.address, 'Event Manager', salaries[1]);
+            const averageSalary = await expect(companyFactory.getAverageSalaryInSector(sector2)).to.be.fulfilled;
+            expect(Number(averageSalary)).to.equal((salaries[0] + salaries[1]) / 2);
         });
     });
 });
